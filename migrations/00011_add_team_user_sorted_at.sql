87065322db00fa4fe05f2e0e496c6f37267f99da
Liberator <fabian.mueller77@gmail.com>
2026-08-23 23:06:14 +0200

    fix migration

--- migrations/00011_add_team_user_sorted_at.sql
+++ migrations/00011_add_team_user_sorted_at.sql
@@ -3,9 +3,8 @@ ALTER TABLE team_users ADD COLUMN sorted_at TIMESTAMPTZ;
 
 UPDATE team_users tu
 SET sorted_at = s.timestamp
-FROM signups s
-JOIN teams t ON t.id = tu.team_id
-WHERE s.event_id = t.event_id AND s.user_id = tu.user_id;
+FROM signups s, teams t
+WHERE t.id = tu.team_id AND s.event_id = t.event_id AND s.user_id = tu.user_id;
 
 UPDATE team_users SET sorted_at = now() WHERE sorted_at IS NULL;
 
